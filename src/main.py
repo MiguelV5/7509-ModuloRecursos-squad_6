cf96d68f552ab9fa1434ff85ef169c06a0b85fb1
Ramiro Gestoso <rgestoso@fi.uba.ar>
2023-06-14 19:07:02 -0300

    agregamos summary a endpoints

--- src/main.py
+++ src/main.py
@@ -24,17 +24,17 @@ def get_db():
 PROYECTOS_BASE_URL = ""
 
 
-@app.get("/")
+@app.get("/", summary="Raíz")
 def root(request: Request):
     return crud.get_mensaje_root()
 
 
-@app.get("/recursos/")
+@app.get("/recursos/", summary="Obtener recursos")
 def getRecursos():
     return crud.get_recursos_desde_endpoint()
 
 
-@app.get("/recursos/{legajo}")
+@app.get("/recursos/{legajo}", summary="Obtener recurso por legajo")
 def getRecursosPorLegajo(legajo: int):
     recurso = crud.get_recurso_por_legajo_desde_endpoint(legajo)
 
@@ -45,16 +45,16 @@ def getRecursosPorLegajo(legajo: int):
     return recurso
 
 
-@app.get("/recursos/allRegistros/")
+@app.get("/recursos/allRegistros/", summary="Obtener todos los registros")
 def getTodosLosRegistrosDeHoras(db: Session = Depends(get_db)):
     return crud.get_all_registros_desde_db(db=db)
 
 
-@app.get("/recursos/{legajo}/registros/")
+@app.get("/recursos/{legajo}/registros/", summary="Obtener registros de un legajo")
 def getRegistrosDeHoras(legajo: int, db: Session = Depends(get_db)):
     return crud.get_registro_por_legajo_desde_db(db=db, legajo=legajo)
 
 
-@app.post("/recursos/{legajo}/registro/", response_model=schemas.RegistroDeHoras)
+@app.post("/recursos/{legajo}/registro/", response_model=schemas.RegistroDeHoras, summary="Crear un registro de un legajo")
 def postRegistroDeHoras(legajo: int, registro: schemas.RegistroDeHorasCreate, db: Session = Depends(get_db)):
     return crud.post_registro(db=db, legajo=legajo, registro=registro)
