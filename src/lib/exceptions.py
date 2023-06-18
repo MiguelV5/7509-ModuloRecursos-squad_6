da250a9b48fe1eef243b3ee2932c88b3d00c3843
Ramiro Gestoso <rgestoso@fi.uba.ar>
2023-06-18 12:43:21 -0300

    agregamos identificador a cada excepcion nuestra

--- src/lib/exceptions.py
+++ src/lib/exceptions.py
@@ -1,12 +1,28 @@
+from enum import Enum, auto
+
+RECURSO_NO_EXISTE = auto()
+REGISTRO_NO_EXISTE = auto()
+FECHA_NO_VALIDA = auto()
+PROYECTO_NO_EXISTE = auto()
+TAREA_NO_EXISTE = auto()
+CANTIDAD_HORAS_NULAS = auto()
+CANTIDAD_HORAS_EXCESIVAS = auto()
+CANTIDAD_HORAS_EXCESIVAS_EN_JORNADA = auto()
+FECHA_INICIAL_MAYOR = auto()
+REGISTRO_VACIO = auto()
+
 class RecursoNoExistenteException(Exception):
     message = ""
+    type = RECURSO_NO_EXISTE
 
     def __init__(self, id):
         self.message = f"Recurso no encontrado: {id}"
+        
 
 
 class RegistroNoExistenteException(Exception):
     message = ""
+    type = REGISTRO_NO_EXISTE
 
     def __init__(self, idRecurso, id):
         self.message = f"Registro de recurso {idRecurso} no encontrado: {id}"
@@ -14,6 +30,7 @@ class RegistroNoExistenteException(Exception):
 
 class FechaNoValidaException(Exception):
     message = ""
+    type = FECHA_NO_VALIDA
 
     def __init__(self):
         self.message = (
@@ -23,6 +40,7 @@ class FechaNoValidaException(Exception):
 
 class ProyectoNoExistenteException(Exception):
     message = ""
+    type = PROYECTO_NO_EXISTE
 
     def __init__(self, idProyecto):
         self.message = f"No existe el proyecto (id dada: {idProyecto}) al que se quiere registrar horas"
@@ -30,6 +48,7 @@ class ProyectoNoExistenteException(Exception):
 
 class TareaNoExistenteException(Exception):
     message = ""
+    type = TAREA_NO_EXISTE
 
     def __init__(self, idTarea):
         self.message = f"No existe la tarea (id dada: {idTarea}) a la que se quieren registrar horas"
@@ -37,6 +56,7 @@ class TareaNoExistenteException(Exception):
 
 class CantidadDeHorasNulasException(Exception):
     message = ""
+    type = CANTIDAD_HORAS_NULAS
 
     def __init__(self):
         self.message = "Cantidad de horas no puede ser negativa o nula"
@@ -44,6 +64,7 @@ class CantidadDeHorasNulasException(Exception):
 
 class CantidadDeHorasExcesivasException(Exception):
     message = ""
+    type = CANTIDAD_HORAS_EXCESIVAS
 
     def __init__(self, maxHoras):
         self.message = f"Cantidad de horas no puede ser mayor a {maxHoras}"
@@ -51,6 +72,7 @@ class CantidadDeHorasExcesivasException(Exception):
 
 class CantidadDeHorasExcesivasEnJornadaException(Exception):
     message = ""
+    type = CANTIDAD_HORAS_EXCESIVAS_EN_JORNADA
 
     def __init__(self, horasACargar, fecha, horasEnFecha, maxHoras):
         self.message = f"Con la cantidad de horas a cargar dada (horas: {horasACargar}), el recurso tendría una cantidad excesiva de horas cargadas en la fecha (fecha: {fecha}, cantidad de horas cargadas previamente en la fecha: {horasEnFecha}, cantidad maxima de una fecha: {maxHoras})"
@@ -58,6 +80,7 @@ class CantidadDeHorasExcesivasEnJornadaException(Exception):
 
 class FechaInicialMayorAFinalException(Exception):
     message = ""
+    type = FECHA_INICIAL_MAYOR
 
     def __init__(self, fechaInicio, fechaFinal):
         self.message = f"La fecha de inicio: {fechaInicio} debe ser menor o igual a la fecha final: {fechaFinal}"
@@ -65,6 +88,7 @@ class FechaInicialMayorAFinalException(Exception):
 
 class RegistroVacioException(Exception):
     message = ""
+    type = REGISTRO_VACIO
 
     def __init__(self, camposModificables):
         self.message = f"Para modificar el registro debe indicar al menos un campo modificable: {camposModificables}"
