7bee07f2ca1cd42a0c2dcd65eb6c2cef88e02068
Ramiro Gestoso <rgestoso@fi.uba.ar>
2023-06-19 15:09:34 -0300

    funciones para borrar registros y crear de forma facil

--- test/features/steps/utils.py
+++ test/features/steps/utils.py
@@ -1,5 +1,8 @@
 from datetime import date, timedelta
 import random
+import requests
+
+BASE_URL = "http://localhost:8000"
 
 def random_date():
     start_date = date.min
@@ -7,3 +10,23 @@ def random_date():
     delta_days = limit_date - start_date
     random_days = random.randint(0, delta_days.days)
     return str(start_date + timedelta(days=random_days))
+
+def borrar_cargas_de_horas(legajo):
+    url = f"{BASE_URL}/recursos/{legajo}/registros"
+    response = requests.get(url)
+    json_response = response.json()
+    for registro in json_response:
+        id = registro["id"]
+        url_delete = f"{BASE_URL}/recursos/{legajo}/registros/{id}"
+        requests.delete(url_delete)
+
+def cargar_horas(legajo):
+    url = f"{BASE_URL}/recursos/{legajo}/registros"
+    for i in range(3):
+        registro = {
+            "cantidad": 2,
+            "fecha_de_registro": random_date(),
+            "id_proyecto": 1,
+            "id_tarea": 1
+        }
+        requests.post(url, json=registro)
